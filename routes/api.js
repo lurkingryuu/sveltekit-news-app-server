e3855b86abf08d385b22d28fda5e01dee61e0c38
Karthikeya <yelisettikarthik0@gmail.com>
2023-10-16 22:01:04 +0530

    Decently working server

--- routes/api.js
+++ routes/api.js
@@ -10,7 +10,7 @@ const crypto = require("crypto").createHash("sha256");
 dotenv.config();
 
 function createHash(password) {
-  return crypto.update(password).copy().digest('hex');
+  return crypto.update(password).copy().digest("hex");
 }
 
 router.use((req, res, next) => {
@@ -29,32 +29,39 @@ router.use((req, res, next) => {
 
 FETCH_URL =
   "https://newsapi.org/v2/everything?" +
-	"q=India&" +
-	"from=2023-09-31&" +
-	"apiKey=" + process.env.API_KEY;
+  "q=India&" +
+  "from=2023-09-31&" +
+  "apiKey=" +
+  process.env.API_KEY;
 
 // GET all news items paginated
 router.get("/news", (req, res, next) => {
-	const page = req.query.page || 1;
-	const limit = req.query.limit || 12;
-	const offset = (page - 1) * limit;
-	db.all("SELECT * FROM news LIMIT ? OFFSET ?", [limit, offset], (err, rows) => {
-    if (err) {
-			res.status(500).json({ error: err.message });
-			return;
-		}
-    res.status(200).json(
-			rows.map((row) => {
-				return {
-					img: row.urlToImage,
-					title: row.title,
-					description: row.description,
-					content: row.content,
-					url: row.url
-				};
-			})
-		);
-  });
+  const page = req.query.page || 1;
+  const limit = req.query.limit || 12;
+  const offset = (page - 1) * limit;
+
+  // sort by publishedAt
+  db.all(
+    "SELECT * FROM news ORDER BY publishedAt DESC LIMIT ? OFFSET ?",
+    [limit, offset],
+    (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.status(200).json(
+        rows.map((row) => {
+          return {
+            img: row.urlToImage,
+            title: row.title,
+            description: row.description,
+            content: row.content,
+            url: row.url,
+          };
+        })
+      );
+    }
+  );
 });
 
 // GET a single news item
@@ -64,94 +71,95 @@ router.get("/news/:id", (req, res, next) => {
       res.status(500).json({ error: err.message });
       return;
     }
-    res.status(200).json(
-			{
-				img: row.urlToImage,
-				title: row.title,
-				description: row.description,
-				content: row.content,
-				url: row.url
-			}
-		);
+    res.status(200).json({
+      img: row.urlToImage,
+      title: row.title,
+      description: row.description,
+      content: row.content,
+      url: row.url,
+    });
   });
 });
 
+let fetched_pages = 0;
+
 // Refresh database with new data
 router.post("/refresh", (req, res, next) => {
-	let count = 0;
-
+  let count = 0;
+  fetched_pages += 1;
+  console.log("Fetching page " + fetched_pages);
   axios
-    .get(FETCH_URL)
+    .get(FETCH_URL + "&page=" + fetched_pages)
     .then((response) => {
       if (response.data.status !== "ok") {
         res.status(500).json({ error: "Error fetching data from News API" });
         return;
       }
 
-			// Create a new table if it does not exist
-			db.run(
-				"CREATE TABLE IF NOT EXISTS news (id TEXT PRIMARY KEY, title TEXT, description TEXT, content TEXT, url TEXT, urlToImage TEXT, publishedAt TEXT)",
-				(err) => {
-					if (err) {
-						console.log(err);
-						res.status(500).json({ error: err.message });
-						return;
-					}
-				}
-			);
+      // Create a new table if it does not exist
+      db.run(
+        "CREATE TABLE IF NOT EXISTS news (id TEXT PRIMARY KEY, title TEXT, description TEXT, content TEXT, url TEXT, urlToImage TEXT, publishedAt TEXT)",
+        (err) => {
+          if (err) {
+            console.log(err);
+            res.status(500).json({ error: err.message });
+            return;
+          }
+        }
+      );
 
-			// Insert each article into the database
+      // Insert each article into the database
       db.serialize(() => {
         response.data.articles.forEach((article) => {
-					count += 1;
-					const url = article.url;
-					// hash the url to get a unique id
-					const id = createHash(url);
-					// check if the article already exists in the database
-					db.get("SELECT * FROM news WHERE id = ?", [id], (err, row) => {
-						if (err) {
-							console.log(err);
-							res.status(500).json({ error: err.message });
-							return;
-						}
-						if (row) {
-							count -= 1;
-							return;
-						}
-						// if the article does not exist, insert it into the database
-						if (!row) {
-							db.run(
-							"INSERT INTO news (id, title, description, content, url, urlToImage, publishedAt) VALUES (?, ?, ?, ?, ?, ?, ?)",
-							[
-								id,
-								article.title,
-								article.description,
-								article.content,
-								article.url,
-								article.urlToImage,
-								article.publishedAt,
-							],
-							(err) => {
-								if (err) {
-									console.log(err);
-									res.status(500).json({ error: err.message });
-									count -= 1;
-									return;
-								}
-							}
-          	);
-					}
+          const url = article.url;
+          // hash the url to get a unique id
+          const id = createHash(url);
+          // check if the article already exists in the database
+          db.get("SELECT * FROM news WHERE id = ?", [id], (err, row) => {
+            if (err) {
+              console.log(err);
+              res.status(500).json({ error: err.message });
+              return;
+            }
+            if (row) {
+              count -= 1;
+              return;
+            }
+            // if the article does not exist, insert it into the database
+            if (!row) {
+              db.run(
+                "INSERT INTO news (id, title, description, content, url, urlToImage, publishedAt) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                [
+                  id,
+                  article.title,
+                  article.description,
+                  article.content,
+                  article.url,
+                  article.urlToImage,
+                  article.publishedAt,
+                ],
+                (err) => {
+                  if (err) {
+                    console.log(err);
+                    res.status(500).json({ error: err.message });
+                    count -= 1;
+                    return;
+                  }
+                }
+              );
+            }
+          });
         });
       });
 
-      console.log(response.data.totalResults);
-      res.status(200).json({
-        message: count? `Database refreshed with ${count} articles` : "No new articles found",
-      });
-    	})
-		})
+      if (fetched_pages * 100 >= response.data.totalResults) {
+        fetched_pages = 0;
+      }
+
+      res.status(200).json({ message: "Database updated" });
+    })
     .catch((error) => {
-			console.log(error);	
+      console.log(error);
       res.status(500).json({ error: error.message });
     });
 });
@@ -160,43 +168,81 @@ router.post("/refresh", (req, res, next) => {
 router.post("/subscribe", (req, res, next) => {
   const email = req.body.email;
   try {
-	db.run(
-		"CREATE TABLE IF NOT EXISTS subscribers (email TEXT PRIMARY KEY)",
-		(err) => {
-		if (err) {
-			console.log(err);
-			res.status(500).json({ error: err.message });
-			return;
-		}
-		}
-	);
+    db.run(
+      "CREATE TABLE IF NOT EXISTS subscribers (email TEXT PRIMARY KEY)",
+      (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({ error: err.message });
+          return;
+        }
+      }
+    );
 
-		// Check if the email already exists in the database   
-	db.get("SELECT * FROM subscribers WHERE email = ?", [email], (err, row) => {
-		if (err) {
-		console.log(err);
-		res.status(500).json({ error: err.message });
-		return;
-		}
-		if (row) {
-		res.status(201).json({ message: "You are already subscribed" });
-		return;
-		}
-		if (!row) {
-		db.run("INSERT INTO subscribers (email) VALUES (?)", [email], (err) => {
-			if (err) {
-			console.log(err);
-			res.status(500).json({ error: err.message });
-			return;
-			}
-			res.status(200).json({ message: "You are now subscribed" });
-		});
-		}
-	});
-	}
-	catch (error) {
-		res.status(500).json({ error: error.message });
-	}
+    // Check if the email already exists in the database
+    db.get("SELECT * FROM subscribers WHERE email = ?", [email], (err, row) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      if (row) {
+        res.status(201).json({ message: "You are already subscribed" });
+        return;
+      }
+      if (!row) {
+        db.run("INSERT INTO subscribers (email) VALUES (?)", [email], (err) => {
+          if (err) {
+            console.log(err);
+            res.status(500).json({ error: err.message });
+            return;
+          }
+          res.status(200).json({ message: "You are now subscribed" });
+        });
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// DELETE a single news item
+router.delete("/news/:id", (req, res, next) => {
+  db.run("DELETE FROM news WHERE id = ?", [req.params.id], (err) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.status(200).json({ message: "News item deleted" });
+  });
+});
+
+// DELETE all news items
+router.delete("/news", (req, res, next) => {
+  db.run("DELETE FROM news", (err) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.status(200).json({ message: "All news items deleted" });
+  });
+});
+
+// Unsubscribe from the newsletter
+router.delete("/unsubscribe", (req, res, next) => {
+  const email = req.body.email;
+  try {
+    db.run("DELETE FROM subscribers WHERE email = ?", [email], (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.status(200).json({ message: "You are now unsubscribed" });
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = router;
